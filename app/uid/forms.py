6a20b6f21b450776bbb857d8559067fbd29ba18e
Trey Hayden <trey.hayden.ctr@adlnet.gov>
2024-11-25 14:20:35 -0500

    adding the changes we lost from merge reversion

--- app/uid/forms.py
+++ app/uid/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Provider, LCVTerm, Alias  # Import Neo4j models directly
+from .models import Provider, LCVTerm, UIDRequestToken # Import Neo4j models directly
+from uuid import uuid4
 #from .models import LastGeneratedUID
 
 #class LastGeneratedUIDForm(forms.ModelForm):
@@ -7,77 +8,65 @@ from .models import Provider, LCVTerm, Alias  # Import Neo4j models directly
   #      model = LastGeneratedUID
    #     fields = ['uid']
 
+# class UIDRequestForm(forms.ModelForm):
+#     token = forms.CharField(max_length=255)
+#     provider_name = forms.CharField(max_length=255)
+#     echelon = forms.CharField(max_length=255)
+#     termset = forms.CharField(max_length=255)
+
+#     def save(self):
+#         given_provider = self.cleaned_data['provider']
+#         given_echelon = self.cleaned_data['echelon']
+#         given_termset = self.cleaned_data['termset']
+
+#         provider_already_exists = Provider.does_provider_exist(given_provider)
+#         if provider_already_exists:
+#             provider = Provider.get_provider_by_name(given_provider)
+#         else:
+#             provider = Provider.create_provider(given_provider)
+        
+#         uid_request = UIDRequestToken()
+#         uid_request.provider_name = provider.name
+#         uid_request.echelon = given_echelon
+#         uid_request.termset = given_termset
+#         uid_request.token = uuid4()
+
+#         uid_request.save()
+
+#         return uid_request
+
+#     class Meta:
+#         model = UIDRequestToken
+#         #fields = ['uid', 'name', 'echelon_level']
+#         fields = ['provider_name', 'echelon', 'termset', 'token'] # UID is self generated
+
 class ProviderForm(forms.ModelForm):
-    uid = forms.CharField(max_length=255)
+    # uid = forms.CharField(max_length=255)
     name = forms.CharField(max_length=255)
 
     def save(self):
-        provider = Provider(uid=self.cleaned_data['uid'], name=self.cleaned_data['name'])
+        name = self.cleaned_data['name']
+        provider = Provider.create_provider(name)
         provider.save()
         return provider
+
     class Meta:
         model = Provider
         #fields = ['uid', 'name', 'echelon_level']
-        fields = ['name', 'echelon_level'] # UID is self generated
+        fields = ['name'] # UID is self generated
 
 class LCVTermForm(forms.ModelForm):
-    uid = forms.CharField(max_length=255)
+    provider_name = forms.CharField(max_length=255)
     term = forms.CharField(max_length=255)
+    echelon = forms.CharField(max_length=255)
+    structure = forms.CharField(max_length=255)
 
     def save(self):
-        lcv_term = LCVTerm(uid=self.cleaned_data['uid'], term=self.cleaned_data['term'])
+        payload = self.cleaned_data
+        lcv_term = LCVTerm.create_term(provider_name=payload["provider_name"], term=payload['term'], echelon_level=payload["echelon"], structure=payload["structure"])
         lcv_term.save()
         return lcv_term
     class Meta:
         model = LCVTerm
         #fields = ['uid', 'term', 'echelon_level']
-        fields = ['term', 'echelon_level'] # UID is self Generated
-        
-# Search Forms
-class SearchForm(forms.Form):
-    search_term = forms.CharField(max_length=255, required=True, label="Search Term")
-    search_type = forms.ChoiceField(choices=[
-        ('general', 'General Search'),
-        ('alias', 'Search by Alias'),
-        ('definition', 'Search by Definition'),
-        ('context', 'Search by Context'),
-    ], required=True, label="Search Type"
-    )
-    context = forms.CharField(label='Context', required=False, max_length=255)
-    
-# Alias with no Context  
-#class AliasForm(forms.Form):
- #   alias = forms.CharField(max_length=255)
-  #  context = forms.CharField(max_length=255, required=False)  # context is optional
-
-#class AliasForm(forms.ModelForm):
- #   class Meta:
-  #      model = Alias
-   #     fields = ['alias', 'context']  # Include alias and context fields
-
-#class AliasForm(forms.Form):
- #   alias = forms.CharField(max_length=255, required=True)
-  #  context = forms.CharField(max_length=255, required=False)
-
-   # def save(self):
-        # Save directly to Neo4j via Neomodel
-    #    alias = Alias(alias=self.cleaned_data['alias'], context=self.cleaned_data.get('context'))
-     #   alias.save()
-      #  return alias
-      
-class AliasForm(forms.Form):
-    alias = forms.CharField(max_length=255, required=True)  # The alias name
-    context = forms.CharField(max_length=255, required=False)  # Context as a string (the term's name)
-
-    def save(self):
-        # Create and save Alias
-        alias = Alias(alias=self.cleaned_data['alias'], context=self.cleaned_data.get('context'))
-        alias.save()
-
-        # Optionally, if context is provided, link to the NeoTerm
-        if alias.context:
-            term = NeoTerm.nodes.get_or_none(name=alias.context)
-            if term:
-                alias.link_to_term(term)  # Link this alias to the found NeoTerm
-
-        return alias
+        fields = ['provider_name', 'term', 'echelon', 'structure'] # UID is self Generated
--- app/uid/views.py
+++ app/uid/views.py
@@ -3,238 +3,84 @@ from django.http import HttpResponse, HttpRequest, JsonResponse
 #from uuid import uuid5, NAMESPACE_URL
 import json, logging
 from neomodel import db
-from .models import UIDGenerator, UIDNode, Provider, LCVTerm, LanguageSet
-from .forms import ProviderForm, LCVTermForm
-from .models import report_all_uids, report_uids_by_echelon, GeneratedUIDLog
+# from .models import UIDGenerator, UIDNode, Provider, LCVTerm, LanguageSet
+from .models import UIDNode, Provider, generate_uid, UIDRequestNode
+from .forms import ProviderForm
+from .models import report_all_uids, report_all_generated_uids, report_all_term_uids, report_uids_by_echelon, GeneratedUIDLog
 from rest_framework import viewsets
 from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
 
-from django.contrib import messages
-import os
-from .forms import SearchForm
-import requests
-import urllib.parse
-from .models import Alias
-from .forms import AliasForm
-
-# Cypher Queries
-SEARCH_BY_ALIAS = """
-WITH toLower($search_term) as search_term
-MATCH (a:NeoAlias)
-WHERE toLower(a.alias) CONTAINS search_term
-MATCH (a)-[:POINTS_TO]->(term:NeoTerm)
-OPTIONAL MATCH (term)-[:POINTS_TO]->(def:NeoDefinition)
-OPTIONAL MATCH (ctx:NeoContext)-[:IS_A]->(term)
-RETURN term.uid as LCVID, a.alias as Alias, def.definition as Definition, ctx.context as Context
-LIMIT 100
-"""
-
-SEARCH_BY_DEFINITION = """
-WITH toLower($search_term) as search_term
-MATCH (def:NeoDefinition)
-WHERE toLower(def.definition) CONTAINS search_term
-MATCH (term:NeoTerm)-[:POINTS_TO]->(def)
-OPTIONAL MATCH (a:NeoAlias)-[:POINTS_TO]->(term)
-OPTIONAL MATCH (ctx:NeoContext)-[:IS_A]->(term)
-RETURN term.uid as LCVID, a.alias as Alias, def.definition as Definition, ctx.context as Context
-LIMIT 100
-"""
-
-SEARCH_BY_CONTEXT = """
-WITH toLower($search_term) as search_term
-MATCH (ctx:NeoContext)
-WHERE toLower(ctx.context) CONTAINS search_term
-MATCH (ctx)-[:IS_A]->(term:NeoTerm)
-OPTIONAL MATCH (term)-[:POINTS_TO]->(def:NeoDefinition)
-OPTIONAL MATCH (a:NeoAlias)-[:POINTS_TO]->(term)
-RETURN term.uid as LCVID, a.alias as Alias, def.definition as Definition, ctx.context as Context
-LIMIT 100
-"""
-
-GENERAL_GRAPH_SEARCH = """
-WITH toLower($search_term) as search_term
-MATCH (n)
-WHERE (n:NeoAlias OR n:NeoDefinition OR n:NeoContext)  
-  AND (
-    (n:NeoAlias AND toLower(n.alias) CONTAINS search_term) OR
-    (n:NeoDefinition AND toLower(n.definition) CONTAINS search_term) OR
-    (n:NeoContext AND toLower(n.context) CONTAINS search_term)
-  )
-WITH n
-CALL {
-    WITH n
-    MATCH path = (n)-[*1..2]-(connected)
-    RETURN path
-}
-RETURN * LIMIT 100
-"""
-
-# Globally Declare variable
-MAX_CHILDREN = 2**32 -1
 
 # Set up logging to capture errors and important information
 logger = logging.getLogger(__name__)
 
-# Attempt to initialize the UID generator
-try:
-    uid_generator = UIDGenerator()
-except RuntimeError as e:
-    # Log an error if UIDGenerator fails to initialize (e.g., due to Neo4j connection issues)
-    logger.error(f"Failed to initialize UIDGenerator: {e}")
-    uid_generator = None  # Handle initialization failure appropriately
+# # Attempt to initialize the UID generator
+# try:
+#     uid_generator = UIDGenerator()
+# except RuntimeError as e:
+#     # Log an error if UIDGenerator fails to initialize (e.g., due to Neo4j connection issues)
+#     logger.error(f"Failed to initialize UIDGenerator: {e}")
+#     uid_generator = None  # Handle initialization failure appropriately
 
-def execute_neo4j_query(query, params):
-    query_str = query
-    try:
-        logger.info(f"Executing query: {query} with params: {params}")
-        results, meta = db.cypher_query(query_str, params)
-        return results
-    except Exception as e:
-        logger.error(f"Error executing Neo4j query: {e}")
-        return None
-
-# Django view for search functionality
-def search(request):
-    results = []
-    if request.method == 'POST':
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            search_term = form.cleaned_data['search_term']
-            search_type = form.cleaned_data['search_type']
-
-            # Log form data for debugging
-            logger.info(f"Search form data: search_term={search_term}, search_type={search_type}")
-
-            # Determine which query to use based on search type
-            if search_type == 'alias':
-                query = SEARCH_BY_ALIAS
-            elif search_type == 'definition':
-                query = SEARCH_BY_DEFINITION
-            elif search_type == 'context':
-                query = SEARCH_BY_CONTEXT
-            else:
-                query = GENERAL_GRAPH_SEARCH  # For 'general' search
-
-            # Log the query and params being sent to Neo4j
-            logger.info(f"Executing query: {query} with params: {{'search_term': {search_term}}}")
-
-            # Execute the query
-            results_data = execute_neo4j_query(query, {"search_term": search_term})
-
-            if results_data:
-                logger.info(f"Raw results data: {results_data}")
-                results = [
-                    {
-                        "LCVID": record[0],  # Assuming record[0] is 'LCVID'
-                        "Alias": record[1],  # Assuming record[1] is 'Alias'
-                        "Definition": record[2],  # Assuming record[2] is 'Definition'
-                        "Context": record[3]  # Assuming record[3] is 'Context'
-                    }
-                    for record in results_data  # Iterating over each record in results_data
-                ]
-            else:
-                logger.info("No results found.")
-                results = [{'error': 'No results found or error querying Neo4j.'}]
-
-    else:
-        form = SearchForm()
-
-    return render(request, 'search.html', {'form': form, 'results': results})
-
-# View for creating an alias
-#def create_alias(request):
- #   if request.method == 'POST':
-  #      form = AliasForm(request.POST)
-   #     if form.is_valid():
-    #        alias_name = form.cleaned_data['alias']
-     #       context = form.cleaned_data.get('context', None)
-
-            # Create the Alias node in Neo4j
-      #      alias = Alias(alias=alias_name, context=context)
-       #     alias.save()  # Save the Alias node to Neo4j
-
-        #    return JsonResponse({'message': 'Alias created successfully!', 'alias': alias_name, 'context': context}, status=201)
-   # else:
-    #    form = AliasForm()
-
-    #return render(request, 'create_alias.html', {'form': form})
-#____________
-#def create_alias(request):
- #   if request.method == 'POST':
-  #      form = AliasForm(request.POST)
-   #     if form.is_valid():
-    #        alias = form.save()  # This will create and save the Alias to Neo4j
-     #       messages.success(request, f"Alias '{alias.alias}' has been successfully created.")
-      #      form = AliasForm()  # Reset the form for another entry if needed
-       # else:
-        #    messages.error(request, "There was an error with the form. Please try again.")
-   # else:
-    #    form = AliasForm()
-
-    #return render(request, 'create_alias.html', {'form': form})
-
-def create_alias(request):
-    if request.method == 'POST':
-        form = AliasForm(request.POST)
-        if form.is_valid():
-            alias = form.save()  # This will create and save the Alias to Neo4j
-            
-            # Check if there was an error with the context linking
-            context_error = getattr(alias, 'context_error', None)
-
-            if context_error:
-                # If there was a context error, show a message
-                messages.error(request, f"Error: {context_error}")
-            else:
-                # If the alias was created successfully, show a success message
-                messages.success(request, f"Warning: No Context provided but Alias '{alias.alias}' has been successfully created.")
-
-            # Reset the form for another entry if needed
-            form = AliasForm()
-
-        else:
-            messages.error(request, "There was an error with the form. Please try again.")
-    else:
-        form = AliasForm()
-
-    return render(request, 'create_alias.html', {'form': form})
-
+MAX_CHILDREN = 2**32 -1
 
 # Create your views here.
 def generate_uid_node(request: HttpRequest):
     request_body = json.loads(request.body)
     print(request_body)
-    strict_parent_validation = request_body.get('strict_parent_validation', False)
+    # strict_parent_validation = request_body.get('strict_parent_validation', False)
     parent_uid = request_body.get('parent_uid', None)
-    namespace = request_body.get('namespace', 'LCV') #??? Ask Hunter about where namespace is actually configured and is it different than just organization?
-    echelon_level = request_body.get('echelon_level', 'level_1')  # Get echelon level from request
+    # namespace = request_body.get('namespace', 'LCV') #??? Ask Hunter about where namespace is actually configured and is it different than just organization?
+    # echelon_level = request_body.get('echelon_level', 'level_1')  # Get echelon level from request
+    # parent_node = UIDNode.get_node_by_uid(parent_uid, namespace, echelon_level='parent_level') #added namespace and parent level
+
+    # if parent_node is None:
+    #     if strict_parent_validation:
+    #         return HttpResponse("{ 'error': 'Parent node not found' }", status=404, content_type='application/json')
+    #     else:
+    #         parent_node = UIDNode.create_node(uid = parent_uid, namespace = namespace)
     
-    parent_node = UIDNode.get_node_by_uid(parent_uid, namespace, echelon_level='parent_level') #added namespace and parent level
+    # num_children = parent_node.children.count()
 
-    if parent_node is None:
-        if strict_parent_validation:
-            return HttpResponse("{ 'error': 'Parent node not found' }", status=404, content_type='application/json')
-        else:
-            parent_node = UIDNode.create_node(uid = parent_uid, namespace = namespace)
+    # # Count children using a loop
+    # num_children = 0
+    # for child in parent_node.children:
+    #    num_children += 1
+
+    # if num_children > MAX_CHILDREN:
+    #     return HttpResponse("{ 'error': 'Max children exceeded for {parent_uid}' }", status=400, content_type='application/json')
     
-    num_children = parent_node.children.count()
+    # local_uid = uid_generator.generate_uid() # updated to use new UID Generation method
+    # local_uid = generate_uid("__ORPHAN_UID_PARENT__")
 
-    # Count children using a loop
-    num_children = 0
-    for child in parent_node.children:
-       num_children += 1
-
-    if num_children > MAX_CHILDREN:
-        return HttpResponse("{ 'error': 'Max children exceeded for {parent_uid}' }", status=400, content_type='application/json')
-    local_uid = uid_generator.generate_uid() # updated to use new UID Generation method
     #local_uid = CounterNode.increment().counter
 
-    new_child_node = UIDNode.create_node(uid = local_uid, namespace = namespace, echelon_level=echelon_level)
+    # new_child_node = UIDNode.create_node(uid = local_uid, namespace = namespace, echelon_level=echelon_level)
+    new_child_node = UIDNode.create_node(parent_uid)
 
-    parent_node.children.connect(new_child_node)
+    # parent_node.children.connect(new_child_node)
 
-    return HttpResponse("{ 'uid': '" + str(local_uid) + "' }", content_type='application/json')
+    return HttpResponse("{ 'uid': '" + str(new_child_node.uid) + "' }", content_type='application/json')
 
+#Potential code to retrieve parent and child nodes using the upstream and downstream capabilities
+#def get_upstream_providers(request, uid):
+    #try:
+      #  lcv_term = LCVTerm.nodes.get(uid=uid)
+     #   upstream_providers = lcv_term.get_upstream()
+    #    upstream_uids = [p.uid for p in upstream_providers]
+   #     return JsonResponse({'upstream_uids': upstream_uids})
+  #  except LCVTerm.DoesNotExist:
+ #       return JsonResponse({'error': 'LCVTerm not found'}, status=404)
+
+#def get_downstream_lcv_terms(request, uid):
+    #try:
+        #provider = Provider.nodes.get(uid=uid)
+       # downstream_lcv_terms = provider.get_downstream()
+      #  downstream_uids = [l.uid for l in downstream_lcv_terms]
+     #   return JsonResponse({'downstream_uids': downstream_uids})
+    #except Provider.DoesNotExist:
+    #    return JsonResponse({'error': 'Provider not found'}, status=404)
 
 # Provider and LCVTerm (Otherwise alternative Parent and child) Now with collision detection on both.
 def create_provider(request):
@@ -242,24 +88,24 @@ def create_provider(request):
         form = ProviderForm(request.POST)
         if form.is_valid():
             provider = form.save()
-            provider.uid = uid_generator.generate_uid()  # Ensure UID is generated
+            # provider.uid = uid_generator.generate_uid()  # Ensure UID is generated
             provider.save()
             return redirect('uid:success')
     else:
         form = ProviderForm()
     return render(request, 'create_provider.html', {'form': form})
 
-def create_lcvterm(request):
-    if request.method == 'POST':
-        form = LCVTermForm(request.POST)
-        if form.is_valid():
-            lcvterm = form.save()
-            lcvterm.uid = uid_generator.generate_uid()  # Ensure UID is generated
-            lcvterm.save()
-            return redirect('uid:success')
-    else:
-        form = LCVTermForm()
-    return render(request, 'create_lcvterm.html', {'form': form})
+# def create_lcvterm(request):
+#     if request.method == 'POST':
+#         form = LCVTermForm(request.POST)
+#         if form.is_valid():
+#             lcvterm = form.save()
+#             lcvterm.uid = uid_generator.generate_uid()  # Ensure UID is generated
+#             lcvterm.save()
+#             return redirect('uid:success')
+#     else:
+#         form = LCVTermForm()
+#     return render(request, 'create_lcvterm.html', {'form': form})
 
 def success_view(request):
     return render(request, 'success.html', {'message': 'Operation completed successfully!'})
@@ -281,6 +127,61 @@ class UIDRepoViewSet(viewsets.ViewSet):
         uids = GeneratedUIDLog.objects.all()
         uid_data = [{'uid': log.uid, 'generated_at': log.generated_at, 'generator_id': log.generator_id} for log in uids]
         return Response(uid_data)
+    
+def report_generated_uids(request):
+    # Retrieve all UIDs from the GeneratedUIDLog model
+    uid_data = report_all_generated_uids()
+    return JsonResponse(uid_data, safe=False)
+
+@csrf_exempt
+def api_generate_uid(request: HttpRequest):
+    if request.method != "POST":
+        return HttpResponse("This endpoint only works with POST.")
+
+    # try:
+    payload = json.loads(request.body.decode("utf-8"))
+    if "provider_name" not in payload:
+        return JsonResponse({"message": "You must specify a 'provider_name' when requesting a UID."}, status=400)
+    
+    given_provider = payload["provider_name"]
+    if not isinstance(given_provider, str):
+        return JsonResponse({"message": "Param 'provider_name' must be a string less than 100 characters long."}, status=400)
+    if len(given_provider) >= 100:
+        return JsonResponse({"message": "Param 'provider_name' must be a string less than 100 characters long."}, status=400)
+
+    if "bulk" in payload:
+        given_bulk = payload["bulk"]
+        if not isinstance(given_bulk, int):
+            return JsonResponse({"message": "Param 'bulk' must be an integer between 0 and 100."}, status=400)
+        if (given_bulk <= 0) or given_bulk > 100:
+            return JsonResponse({"message": "Param 'bulk' must be an integer between 0 and 100."}, status=400)
+        
+        request_nodes = [UIDRequestNode.create_requested_uid(given_provider) for _ in range(given_bulk)]
+        return JsonResponse([
+            {
+                "token": node.token,
+                "uid": node.default_uid,
+                "uid_chain": node.default_uid_chain
+            }
+            for node in request_nodes
+        ], safe=False)
+        
+    else:
+        request_node = UIDRequestNode.create_requested_uid(given_provider)
+        return JsonResponse({
+            "token": request_node.token,
+            "uid": request_node.default_uid,
+            "uid_chain": request_node.default_uid_chain
+        })
+    
+    # except Exception as ex:
+    #     return HttpResponse(f"Could not process request: {ex}")
+
+class UIDTermViewSet(viewsets.ViewSet):
+    def list(self, request):
+        # Retrieve all UIDs from the GeneratedUIDLog model
+        uid_data = report_all_term_uids()
+        return JsonResponse(json.dumps(uid_data, default=str))
     
 # Postman view
 def export_to_postman(request, uid):
